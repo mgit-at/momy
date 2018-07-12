b77da2b5766f0735d7d0a083b7afa61df25949ad
Martin Buchleitner <mbuchleitner@mgit.at>
2018-07-12 08:07:07 +0200

    cleanup code for fast inserts into mongodb

--- bin/momy.js
+++ bin/momy.js
@@ -7,10 +7,12 @@ const fs = require('fs')
 
 const DEFAULT_CONFIG_PATH = 'momyfile.json'
 const refresh = process.argv.some(c => c === '--import')
+const forever = process.argv.some(c => c === '--forever')
 const finder = (p, c, i, a) => c === '--config' && a[i + 1] ? a[i + 1] : p
 const file = process.argv.reduce(finder, DEFAULT_CONFIG_PATH)
-const config = JSON.parse(fs.readFileSync(process.cwd() + '/' + file))
+const config = JSON.parse(fs.readFileSync(file))
 const tailer = new Tailer(config)
 
-if (refresh) tailer.importAndStart()
-else tailer.start()
+if (refresh) return tailer.importAndStart(forever)
+
+tailer.start(forever)
